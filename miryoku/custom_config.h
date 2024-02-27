1f8c6ea07af44f32cc1542afbb92dbebaf22d120
Miles Hoover <mhoover708@gmail.com>
2024-02-26 20:12:25 -0500

    fix

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,9 +1,9 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#MIRYOKU_ALPHAS=COLEMAKDHK
-
 #if !defined (MIRYOKU_LAYOUTMAPPING_CORNE)
+
+##MIRYOKU_ALPHAS=COLEMAKDHK
 
 #define XXX &none
 
